1db692e0b79f8676763e0a12532854a1db63655f
yPalta <103906802+yPalta@users.noreply.github.com>
2024-04-03 15:16:42 +0000

    asd

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ using namespace std;
 int main(){
     
     cout<<"Hola perkin"<<endl;
+    cout<<"Hola perkin"<<endl;
+    cout<<"Hola perkin"<<endl;
+    cout<<"Hola perkin"<<endl;
+    cout<<"Hola perkin"<<endl;
+    cout<<"Hola perkin"<<endl;
+    cout<<"Hola perkin"<<endl;
 
     return 0;
 }
